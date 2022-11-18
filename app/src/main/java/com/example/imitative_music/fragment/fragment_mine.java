9450e9bc0bf4affa6e18a5c8a70ecc0b45c45a31
theonion <1554367213@qq.com>
2022-11-18 21:06:38 +0800

    完成我的界面下方tablayout和viewpager2的编写

--- app/src/main/java/com/example/imitative_music/fragment/fragment_mine.java
+++ app/src/main/java/com/example/imitative_music/fragment/fragment_mine.java
@@ -4,17 +4,57 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.imitative_music.R;
+import com.example.imitative_music.community.MyViewAdapter;
+import com.example.imitative_music.mine.chuangjiangedan;
+import com.example.imitative_music.mine.gedanzhushou;
+import com.example.imitative_music.mine.shoucanggedan;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class fragment_mine extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private List<String> tablayoutList = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_layout_mine,container,false);
+        initFragment();
+        View view = inflater.inflate(R.layout.fragment_layout_mine, container, false);
+
+        viewPager2 = view.findViewById(R.id.viewpager2);
+        tabLayout = view.findViewById(R.id.tabLayout);
+        MyViewAdapter myViewAdapter = new MyViewAdapter(getChildFragmentManager(), getLifecycle(), fragmentList);
+        viewPager2.setAdapter(myViewAdapter);
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(tablayoutList.get(position));
+            }
+        }).attach();
+        return view;
+    }
+
+    private void initFragment() {
+        fragmentList.add(new chuangjiangedan());
+        fragmentList.add(new shoucanggedan());
+        fragmentList.add(new gedanzhushou());
+        tablayoutList.add("创建歌单");
+        tablayoutList.add("收藏歌单");
+        tablayoutList.add("歌单助手");
     }
 }
---
+++ app/src/main/java/com/example/imitative_music/mine/gedanzhushou.java
@@ -0,0 +1,48 @@
+package com.example.imitative_music.mine;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.imitative_music.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class gedanzhushou extends Fragment {
+
+
+    List<list_bean> list;
+    RecyclerView recyclerView;
+    CreatesonglistAdapter adapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        list= new ArrayList<>();
+        init();
+        View view= inflater.inflate(R.layout.mine_chuangjiangedan, container, false);
+        recyclerView=view.findViewById(R.id.rv_create1);
+        LinearLayoutManager layout=new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layout);
+        adapter=new CreatesonglistAdapter(list);
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
+    private void init() {
+        list_bean list_beans=new list_bean("歌单助手","10首",R.mipmap.minr_gedantu);
+        list.add(list_beans);
+        list.add(list_beans);
+        list.add(list_beans);
+        list.add(list_beans);
+        list.add(list_beans);
+        list.add(list_beans);
+        list.add(list_beans);
+    }
+}
